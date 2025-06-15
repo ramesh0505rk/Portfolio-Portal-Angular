41ec0348f4bfa52a25c48ecacb5eecd0835b76a3
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-06-15 20:33:51 +0530

    Implemented gsap & lenis setup in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, NgZone } from '@angular/core';
+import gsap from 'gsap';
+import Lenis from 'lenis';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,42 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  constructor(private ngZone: NgZone) { }
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    this.ngZone.runOutsideAngular(() => {
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: '.animated-element',
+          start: 'start center',
+          end: 'bottom center',
+          scrub: false,
+          markers: true,
+          toggleActions: 'play reverse play reverse',
+        },
+      }).to('.animated-element', {
+        x: 750,
+        rotation: 360,
+        scale: 2.5,
+      });
+
+      const lenis = new Lenis();
+      lenis.on('scroll', (e: any) => {
+        console.log('Lenis Scroll:', e);
+      });
+
+      const raf = (time: number) => {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      };
+
+      requestAnimationFrame(raf);
+    });
+
+  }
 }
